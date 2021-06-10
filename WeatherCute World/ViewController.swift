cae0fe69f96661fbf22cbee28333c637ee132bb2
Kate <kduncanwelke@gmail.com>
2021-06-10 13:28:32 -0500

    Initial commit

---
+++ WeatherCute World/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherCute World
+//
+//  Created by Kate Duncan-Welke on 6/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
